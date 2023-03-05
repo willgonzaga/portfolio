f47fdeeaec3390c2b9a2c64bc1ce4af7bd8f3b57
vailei <willey.oliveira12@gmail.com>
2023-03-05 16:42:32 -0300

    portfolio changes

--- index.js
+++ index.js
@@ -6,13 +6,80 @@ const path = require("path");
 const dotenv = require('dotenv');
 dotenv.config();
 const moment = require('moment-timezone')
+const { MongoClient } = require('mongodb');
+const url = process.env.linkDB;
+const client = new MongoClient(url);
+const dbName = 'Portfolio';
 
 app.use(express.static(join(__dirname + '/public')));
 app.set("views", path.join(__dirname + "/public/views"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("index");
+    async function main() {
+            
+        await client.connect();
+        console.log('Acessando banco de dados...');
+        const db = client.db(dbName);
+        const collection = db.collection('Projetos');
+
+        var dbArray = await collection.find({}).toArray();
+        var cont = 0;
+        var projetos = new Array();
+        while(cont<dbArray.length) {
+            var obj = {
+                "id": dbArray[cont].id,
+                "projname": dbArray[cont].projname,
+                "projdesc": dbArray[cont].projdesc,
+                "projimg": dbArray[cont].projimg,
+                "link": dbArray[cont].link,
+                "repositorio": dbArray[cont].repositorio,
+                "linguagens": dbArray[cont].linguagens
+            }
+            projetos.unshift(obj);
+            cont++;
+        }
+
+        res.render("index", {projetos: projetos});
+        return 'Saindo do banco de dados...';
+    }
+
+    main()
+		.then(console.log)
+		.catch(console.error)
+		.finally(() => client.close());
+})
+
+app.get("/projects/:projid", function (req, res) {
+    var projid = req.params.projid;
+    async function main() {
+            
+        await client.connect();
+        console.log('Acessando banco de dados...');
+        const db = client.db(dbName);
+        const collection = db.collection('Projetos');
+
+        var dbArray = await collection.find({id: projid}).toArray();
+
+        var obj = {
+            "id": dbArray[0].id,
+            "projname": dbArray[0].projname,
+            "projdesc": dbArray[0].projdesc,
+            "projimg": dbArray[0].projimg,
+            "link": dbArray[0].link,
+            "repositorio": dbArray[0].repositorio,
+            "linguagens": dbArray[0].linguagens
+        }
+
+        res.render("projects", {projeto: obj});
+
+        return 'Saindo do banco de dados...';
+    }
+
+    main()
+		.then(console.log)
+		.catch(console.error)
+		.finally(() => client.close());
 })
 
 app.get("/sitemap.xml", function (req, res) {
