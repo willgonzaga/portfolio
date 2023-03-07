e78462606e8bd7c687f63da2314221927183c1f2
vailei <willey.oliveira12@gmail.com>
2023-03-07 02:47:30 -0300

    teste3

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ app.get("/", function (req, res) {
 		.then(console.log)
 		.catch(console.error)
 		.finally(() => client.close());
-res.render("index", {projetos: projetos});
 })
 
 app.get("/projects/", function (req, res) {
