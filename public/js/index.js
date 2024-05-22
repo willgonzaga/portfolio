9b015bc87838b212e95ee0d73e55f1d556f7aa8d
vailei <willey.oliveira12@gmail.com>
2024-05-22 00:43:49 -0300

    Change developer role

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
 var typed = new Typed('.type-h2', {
-    strings: ['&#60;&#47;' +'Willey Gonzaga de Oliveira' + '&#62;'],
+    strings: ['&#60;&#47;' + 'Willey Gonzaga de Oliveira' + '&#62;'],
     typeSpeed: 30,
 });
 
 var typed2 = new Typed('.type-p1', {
-    strings: ['Sou um <b>Desenvolvedor Back-End</b>!'],
+    strings: ['Sou um <b>Desenvolvedor de Software</b>!'],
     typeSpeed: 25,
     startDelay: 1500,
     showCursor: true,
